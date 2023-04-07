79a9791b1798516925c737adc4250149c5a858a8
AleksArden <i1723@i.ua>
2023-04-07 12:35:41 +0300

    add sendgrid

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 const nodemailer = require('nodemailer')
-const { BASE_URL } = process.env
+const { BASE_URL, SENDGRID_USERNAME, SENDGRID_APIKEY, FROM } = process.env
+
+
 
 const config = {
-    host: 'smtp.mailgun.org',
-    port: 587,
+    service: 'SendGrid',
     auth: {
-        user: 'postmaster@sandbox7db6176c9e8c4b0a83ea4bd57458f324.mailgun.org',
-        pass: '07ea354a2260907d4e54fb8782f96e0c-81bd92f8-34993745',
+        user: SENDGRID_USERNAME,
+        pass: SENDGRID_APIKEY,
     }
 }
 const transport = nodemailer.createTransport(config)
@@ -17,13 +18,13 @@ const sendEmail = async (user) => {
     const dataVerifyEmail = {
         to: user.email,
 
-        from: "Mailgun Sandbox <postmaster@sandbox7db6176c9e8c4b0a83ea4bd57458f324.mailgun.org>",
+        from: `Admin <${FROM}>`,
 
         subject: 'Verify email',
 
         text: `Please, confirm your email: ${BASE_URL}/user/verify/${user.verificationToken}`,
 
-        html: `<p> Please, confirm your email<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click verify email</a></p>`,
+        html: `<p> Please, confirm your email. <a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click verify email</a></p>`,
     }
 
     try {
